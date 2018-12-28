d64e2a9f79f1c8196bea02795804e8724a15527c
AbigFUZZYbunny <cconaway9@gmail.com>
2018-12-27 17:07:39 -0800

    Added TM Library
    Added the TM library and created the defines.h file.

---
+++ Inc/defines.h
@@ -0,0 +1,15 @@
+/*
+ * defines.h
+ *
+ *  Created on: Dec 27, 2018
+ *      Author: MCT E560 Marc
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#ifndef STM32F4XX
+#define STM32F4XX
+#endif
+
+#endif /* DEFINES_H_ */
